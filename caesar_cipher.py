62613757421c768fcabf544a78360e950141f78d
Arsenii Hlazov <glazov.arseniy@gmail.com>
2022-03-04 17:41:32 +0200

    Clean code

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -16,25 +16,27 @@ def encrypt(text, mode, lang, shift):
     if mode == 'e':
         pass
     elif mode == 'd':
-        shift = -(shift)
+        shift = -shift
     
     if lang.lower() == 'en':
-        for c in text:
-            if c.isalpha() and c.islower():
-                text_encoded = text_encoded + chr(ord('a') + (ord(c) - ord('a') + shift) % 26)
-            elif c.isalpha() and c.isupper():
-                text_encoded = text_encoded + chr(ord('A') + (ord(c) - ord('A') + shift) % 26)
-            else:
-                text_encoded = text_encoded + c
-    
-    if lang.lower() == 'ru':
-        for c in text:
-            if c.isalpha() and c.islower():
-                text_encoded = text_encoded + chr(ord('а') + (ord(c) - ord('а') + shift) % 32)
-            elif c.isalpha() and c.isupper():
-                text_encoded = text_encoded + chr(ord('А') + (ord(c) - ord('А') + shift) % 32)
-            else:
-                text_encoded = text_encoded + c
+        starting_char_uppercase = 'A'
+        starting_char_lowercase = 'a'
+        alphabet_count = 26
+    elif lang.lower() == 'ru':
+        starting_char_uppercase = 'А'
+        starting_char_lowercase = 'а'
+        alphabet_count = 32
+
+    for c in text:
+        if c.isalpha() and c.islower():
+            text_encoded = text_encoded + chr(ord(starting_char_lowercase) + 
+            (ord(c) - ord(starting_char_lowercase) + shift) % alphabet_count)
+        elif c.isalpha() and c.isupper():
+            text_encoded = text_encoded + chr(ord(starting_char_uppercase) + 
+            (ord(c) - ord(starting_char_uppercase) + shift) % alphabet_count)
+        else:
+            text_encoded = text_encoded + c
+
     return text_encoded
 
 def len_text(text):
